5235725e979a2df3e01174401ca8735557210126
ramonunibz <ramonpiha@hotmail.it>
2026-01-05 11:47:27 +0100

    fixed seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-export default {
-  integrations: [react()],
+import sitemap from "@astrojs/sitemap";
+export default defineConfig({
+  integrations: [react(), sitemap()],
   trailingSlash: "ignore",
   redirects: {
     '/': '/it',
   },
-};
+});
